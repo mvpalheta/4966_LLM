30b55e7a9ff9b7eb724d244a3fa645e62f7cfa25
mvpalheta <mvpalheta@gmail.com>
2023-10-24 00:51:33 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# 4966_LLM
- Assistente virtual com informações sobre as resoluções 4.966 e 309 do Banco Central do Brasil e relacionadas, utilizando LLamaIndex com o modelo gpt-3.5-turbo
+# Converse com as resoluções 4.966/2021 e 309/2023 do Bacen
+Este repositório tem como finalidade compartilhar os códigos utilizados no desenvolvimento de um pequeno exercício de utilização de modelos LLM. Neste caso, foi desenvolvida uma aplicação onde é possível "conversar" com as resoluções 4.966/2021 e 309/2023 do Banco Central do Brasil. Estas resoluções dispõem sobre os novos conceitos e os critérios contábeis aplicáveis a instrumentos financeiros, que irão vigorar a partir de 1º de janeiro de 2025, sendo de extrema importância para o Sistema Financeiro Nacional.
+
+No caso da aplicação apresentada neste repositório, foi utilizada o framework Llamaindex em conjunto com o LLM gpt-3.5-turbo. Llamaindex é um framework simples e flexível que permite conectar fontes de dados privadas e/ou customizadas a diversos LLMs. O resultado disto é que as respostas fornecidas pelos LLMs são mais relevantes e específicas ao contexto de negócio desejado. No caso deste exercício foi utilizado o framework Llamaindex em conjunto com o LLM gpt-3.5-turbo.
+
+O exercício se torna mais interessante tendo em vista que o LLM gpt-3.5-turbo foi treinado com dados anteriores à divulgação das resoluções utilizadas. Sendo assim, até o momento o ChatGPT público não possui  informações relativas a elas, não sendo capaz de responder questionamentos sobre as resoluções 4.966 e 309.
+
+Neste [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/) você encontra um tutorial sobre como construir este tipo de app. 
---
+++ main_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
+st.set_page_config(page_title="Converse com Resoluções do Bacen, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets.openai_key
+st.header("Converse com as Resoluções 4.966 e 309 do Banco Central e outras relacionadas, powered by LlamaIndex 💬🦙")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Me pergunte algo relacionado às Resoluções 4.966 e 309 do Banco Central!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Sua pergunta"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Pensando..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
